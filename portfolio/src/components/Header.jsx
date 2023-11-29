0d05b3a73a28d47db990ca9d11727a5c17a08f33
pcrainer38 <pammie_78704@yahoo.com>
2023-11-28 22:43:07 -0600

    initial commit

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+
+const Header = () => {    
+    return ( 
+    <>
+        <div className='container .d-flex '>
+                    <h1>Pamela Crainer</h1>
+                    <h3>About</h3>
+                    <h3>Portfolio</h3>
+                    <h3>Resume</h3>
+                    <h3>Contact</h3>
+        
+        </div>
+    </>
+    )
+}
+
+export default Header;

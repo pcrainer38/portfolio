27277a5aa41b33bb17a62c53a52d340997c165d8
pcrainer38 <pammie_78704@yahoo.com>
2023-11-28 23:13:10 -0600

    added additional component files

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -3,13 +3,16 @@
 const Header = () => {    
     return ( 
     <>
-        <div className='container .d-flex '>
-                    <h1>Pamela Crainer</h1>
-                    <h3>About</h3>
-                    <h3>Portfolio</h3>
-                    <h3>Resume</h3>
-                    <h3>Contact</h3>
-        
+        <div className='container d-flex p-2 .align-items-center'>
+            <div>
+                <h1>Pamela Crainer</h1>
+            </div>
+            <div class='navbar .justify-content-end .mx-5 .align-items-center '>
+                    <h4><a href='#'>About</a></h4>
+                    <h4><a href='#'>Portfolio</a></h4>
+                    <h4><a href='#'>Resume</a></h4>
+                    <h4><a href='#'>Contact</a></h4>
+            </div>
         </div>
     </>
     )

52ecb1357409a67615ba9704bb0e539d59583036
pcrainer38 <pammie_78704@yahoo.com>
2023-12-11 12:15:02 -0600

    added project 3

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,5 +1,6 @@
 import img1 from '../../assets/Concert-Tracker.png';
 import img2 from '../../assets/pexels-tima-miroshnichenko-7991579.jpg';
+import img3 from '../../assets/MoneyType.png';
 import { FaGithub } from "react-icons/fa";
 
 const Project = () => {
@@ -9,13 +10,17 @@ const Project = () => {
             <div className='container d-flex m-5' id='project'>
                 <div className='img '>
                     <div className='container-sm'>
-                        <img src={img1} alt='concert' className='img1 mt-2 mx-auto' />
+                        <img src={img1} alt='concert' className='img1 mt-5 mx-auto' />
                         <h5><a href='https://bfinch3.github.io/Concert-Tracker/' target="_blank">Concert/Event Tracker </a>
                         <a href='https://github.com/Bfinch3/Concert-Tracker' target='_blank'><FaGithub /></a></h5>
 
                         <img src={img2} alt='movie theater' className='img2 mt-5' />
                         <h5><a href='https://movie-time.dev.133700.xyz/' target="_blank">Movie Time </a>
                         <a href='https://github.com/ashoener/movie-time' target='_blank'><FaGithub /></a></h5>
+
+                        <img src={img3} alt='money type homepage' className='img2 mt-5' />
+                        <h5><a href='https://moneytype.dev.133700.xyz/' target="_blank">Money Type </a>
+                        <a href='https://github.com/pcrainer38/moneytype' target='_blank'><FaGithub /></a></h5>
 
                     </div>
                 </div>
